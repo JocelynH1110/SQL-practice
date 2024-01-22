cfcf657974e1c69d830d3cce83e49c5b5b8c2470
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-01-23 00:39:54 +0800

    INSERT SELECT 和 SELECT INTO 的用法

--- 20240122 插入數據.sql
+++ 20240122 插入數據.sql
@@ -118,4 +118,45 @@ FROM custNew;
 --新例子說明：
 --從 custNew 表中讀取數據並插入到 customers 表。要驗證是否能成功必須先創建 custNew 表並輸入值。在輸入值時，不應該使用已在 customers 中用過的 cust_id 值（主鍵值重複，後續的 INSERT 將會失敗）。
 
+--分析：
+--上面例子使用INSERT SELECT 從 custNew 中將所有數據導入 customers。
+--如果 custNew 如果 custNew 表是空的，則沒有行被插入（也不產生錯誤，因為操作仍是合法的）
+--如果這表有數據，則所有數據將被插入到 customers。
 
+--Tip INSERT SELECT 中的列名：
+--此例子INSERT 和 SELECT 語句中使用了相同的列名。
+--但不一定要求列名匹配，DBMS 一點都不關心 SELECT 返回的列名，因為它使用的是列的位置。
+--故 SELECT 中的第一列（不管其列名）將用來填充表列中指定的第一列，等等。
+
+--Tip 插入多行
+--INSERT 通常只插入一行。
+--插入多行，必須執行多個 INSERT 語句。
+--INSERT SELECT 是例外，它可用一條 INSERT 插入多行，不管 SELECT 語句返回多少行，都將被 INSERT 插入。
+
+
+--2.從一個表複製到另一個表
+--將一個表的內容複製到一個全新的表（運行中創建的表），可以使用 SELECT INTO 語句。（不使用 INSERT 語句）
+--INSERT SELECT：將數據添加到一個已存在的表
+--SELECT INTO：將數據複製到一個新表（有的 DBMS 可以覆蓋已存在的表，這依賴於所使用的具體 DBMS）。
+--重要差別是前者導出數據，後者導入數據。
+
+SELECT *
+INTO custCopy
+FROM customers;
+
+--另一種寫法
+CREATE TABLE custCopy AS
+SELECT * FROM customers;
+
+--分析：
+--SELECT 語句創建一個名為 custCopy 的新表，並把 customers 表的整個內容複製到新表中。
+--因為這裡用 SELECT *，所以將在 custCopy 表中創建（並填充）與 customers 表的每一列相同的列。
+--若只要複製某部份的列，可以明確給出列名，取代* 。
+
+--使用 SELECT INTO 時，須知：
+--任何 SELECT 選項和子句都可以使用，包括 WHERE 和 GROUP BY。
+--可利用聯結從多個表插入數據。
+--不管從多少表中檢索數據，數據都只能插入到一個表中。
+
+--Tip 進行表的複製
+--在測試新的 SQL 語句前，可以使用 SELECT INTO 進行複製，在複製的數據上測試代碼就不會影響實際的數據了。
