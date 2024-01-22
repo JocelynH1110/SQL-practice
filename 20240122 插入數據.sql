16666438c9ca11a8c197b956d33bd0d70e5aac93
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-01-22 12:21:14 +0800

    INSERT-數據插入-插入部份行、插入檢索出的數據

--- 20240122 插入數據.sql
+++ 20240122 插入數據.sql
@@ -53,11 +53,69 @@ VALUES ('1000000006',
         NULL,
         NULL);
 
---分：
+--分析：
 --表名後括號裡明確給出列名。值會帶進相對應的列名。
---列名換順序值跟著換就不會有順序上的問題了。
+--列名換順序而值跟著換就不會有順序上的問題了。
 
 --Note 小心使用 VALUES：
 --不管使用哪種 INSERT 語句，VALUES 的數目都必須正確。
 --若不提供列名，則必須給每個表列提供一個值
 --若提供列名，則必須給列出的每個列一個值。否則會產生錯誤訊息，相應的行不能成功插入。
+
+--1.2 插入部份行
+INSERT INTO customers(cust_id,
+                      cust_name,
+                      cust_address,
+                      cust_city,
+                      cust_state,
+                      cust_zip,
+                      cust_country)
+VALUES ('1000000006',
+        'Toy Land',
+        '123 Any Street',
+        'New York',
+        'NY',
+        '11111',
+        'USA');
+ 
+--分析：
+--由於有兩個值是空值，故沒有 INSERT 必要，可以直接省略。
+
+--Note 省略列
+--省略的列必須滿足以下某個條件:
+--該列定義允許 NULL 值（無值或空值）。
+--在表定義中給出預設值。這表示如果不給出值，將使用預設值。
+
+--Note 省略所需的值
+--如果表中不允許有 NULL 值或預設值，這時卻忽略了表中的值，DBMS 將會產生錯誤消息，無法成功插入。
+
+--1.3 插入檢索出的數據
+--INSERT 一般用來給表插入具有指定列值的行。
+--另一個型式可以用它將 SELECT 語句的結果插入表中，所謂的 INSERT SELECT。
+--顧名思義，它是由一條 INSERT 語句和一條 SELECT 語句組成的。
+
+--例子，想把另一個表中的顧客列合併到 customers 表中：
+INSERT INTO customers(cust_id,
+                      cust_contact,
+                      cust_email,
+                      cust_name,
+                      cust_address,
+                      cust_city,
+                      cust_state,
+                      cust_zip,
+                      cust_country)
+SELECT cust_id,
+       cust_contact,
+       cust_email,
+       cust_name,
+       cust_address,
+       cust_city,
+       cust_state,
+       cust_zip,
+       cust_country
+FROM custNew;
+
+--新例子說明：
+--從 custNew 表中讀取數據並插入到 customers 表。要驗證是否能成功必須先創建 custNew 表並輸入值。在輸入值時，不應該使用已在 customers 中用過的 cust_id 值（主鍵值重複，後續的 INSERT 將會失敗）。
+
+
